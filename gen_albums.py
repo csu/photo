9e30791b869a85bdeaa0f9ea8d841e21cb9caf8d
Christopher Su <christophersu9@gmail.com>
2015-12-16 17:00:19 -0800

    album generation

---
+++ gen_albums.py
@@ -0,0 +1,14 @@
+import json
+
+with open('albums.json') as data_file:
+  data = json.load(data_file)
+
+for slug, options in data['albums'].iteritems():
+  f = open('%s.html' % slug, 'w')
+  f.write('---\n')
+
+  for k, v in options.iteritems():
+    f.write('%s: %s\n' % (k, v))
+
+  f.write('---')
+  f.close()
